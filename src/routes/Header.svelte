39ee64861fd281121a0198eb82c8dd3a6e681eda
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-10-02 06:43:56 -0600

    Ensure build works without SSR  - Removed invalid entries (the game and the about page)  - Added dynamic routes to config  - Moved back to root

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -25,9 +25,6 @@
 			<li aria-current={/\/teams\/?.*$/.test($page.url.pathname) ? 'page' : undefined}>
 				<a href="/teams">Team building</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
